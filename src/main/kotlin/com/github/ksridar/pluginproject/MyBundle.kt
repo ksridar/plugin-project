8eabf48acfcb97cdd679fd580b1af0ecab51ef3d
GitHub Action <action@github.com>
2020-12-30 01:53:30 +0000

    Template cleanup

--- src/main/kotlin/com/github/ksridar/pluginproject/MyBundle.kt
+++ src/main/kotlin/com/github/ksridar/pluginproject/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.ksridar.pluginproject
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/ksridar/pluginproject/services/MyProjectService.kt
+++ src/main/kotlin/com/github/ksridar/pluginproject/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.ksridar.pluginproject.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.ksridar.pluginproject.MyBundle
 
 class MyProjectService(project: Project) {
 
